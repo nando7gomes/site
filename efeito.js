79ea81f8d30b57c0156314ab5e5235e90b60f771
Nand0G0mes <fernando.m.gomes@hotmail.com>
2023-03-08 18:00:05 +0100

    a

---
+++ efeito.js
@@ -0,0 +1,34 @@
+// seleciona todas as divs com a classe "box"
+const boxes = document.querySelectorAll('.box');
+
+// adiciona um listener de rolagem na janela
+window.addEventListener('scroll', () => {
+  // para cada div, verifica se ela está na área visível da janela
+  boxes.forEach(box => {
+    const boxTop = box.getBoundingClientRect().top;
+    const boxBottom = box.getBoundingClientRect().bottom;
+    const windowHeight = window.innerHeight;
+    
+    if (boxTop < windowHeight && boxBottom >= 0) {
+      // se a div estiver na área visível, adiciona a classe "show"
+      box.classList.add('show');
+
+      // adiciona a classe de transição correspondente
+      if (box.classList.contains('type-1')) {
+        box.classList.add('type-1-show');
+      } else if (box.classList.contains('type-2')) {
+        box.classList.add('type-2-show');
+      }
+    } else {
+      // se não estiver na área visível, remove a classe "show"
+      box.classList.remove('show');
+
+      // remove a classe de transição correspondente
+      if (box.classList.contains('type-1')) {
+        box.classList.remove('type-1-show');
+      } else if (box.classList.contains('type-2')) {
+        box.classList.remove('type-2-show');
+      }
+    }
+  });
+});
